6f4adecdc0e5daae0fcfb80e88dcaffa5cf8da16
Kevin Lee <kevin.code@lckymn.com>
2013-10-13 02:53:09 +1100

    *Removed -Unnecessary Java source files.

--- app/com/lckymn/kevin/emailextractor/service/EmailAddressExtractor.scala
+++ app/com/lckymn/kevin/emailextractor/service/EmailAddressExtractor.scala
@@ -16,26 +16,6 @@ import com.lckymn.kevin.emailextractor.AbstractExtractor
  */
 case class SimpleEmailAddressExtractor(val pattern: Regex) extends AbstractExtractor[EmailAddress] {
   override def extract(value: String): Set[EmailAddress] = {
-    // # first
-    //    (for {
-    //      s <- value.split("[\\s]+").toSet[String]
-    //      matcher = pattern.matcher(s)
-    //      if (matcher.find)
-    //      email = Option(matcher.group(0)).getOrElse("").trim
-    //      if (!email.isEmpty())
-    //    } yield EmailAddress(email)).toSet
-
-    // # second
-    //    (for {
-    //      email <- pattern.findAllIn(value)
-    //      if (!email.isEmpty())
-    //    } yield EmailAddress(email)).toSet
-
-    // # third
-    //    pattern.findAllIn(value)
-    //      .toSet[String]
-    //      .map((email) => EmailAddress(email))
-
     extract(value, (email) => EmailAddress(email))
   }
 }
